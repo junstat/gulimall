d53b12cb146980a851795ace2baa63779091763e
junstat <hejunstat@163.com>
2023-10-19 21:56:56 +0800

    项目初始化

---
+++ gulimall-member/src/test/java/com/jun/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jun.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
